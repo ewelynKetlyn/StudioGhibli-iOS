22c38c8b5d55b21420388364d25aeb64ff2d0473
ewelynsilva <ewelyn.silva@hoamtechnology.com>
2026-02-28 21:07:36 -0300

    Initial Commit

---
+++ StudioGhibli/StudioGhibliApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudioGhibliApp.swift
+//  StudioGhibli
+//
+//  Created by Ewelyn Silva on 28/02/26.
+//
+
+import SwiftUI
+
+@main
+struct StudioGhibliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
